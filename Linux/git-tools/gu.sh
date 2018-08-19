c907ce4c0b46d53c3b71f95c3ac564cd1d688edd
Dillon <dillon-a@hotmail.com>
2018-08-19 17:56:07 -0500

    adding linux git update script

---
+++ Linux/git-tools/gu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Git Update
+#
+# This script is intended to update the current working branch from master (or a specified branch)
+#
+
+# if [ -z "$1" ] then
+#     TARGET_BRANCH="master"
+# else
+#     TARGET_BRANCH=$1
+# fi
+
+CURRENT_BRANCH=$('git rev-parse --abbrev-ref HEAD')
+
+git checkout master #$TARGET_BRANCH
+git pull
+git checkout $CURRENT_BRANCH
+git merge master #$TARGET_BRANCH
