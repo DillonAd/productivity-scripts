0a152635d51ac553195610a8eebfb9729b83f1aa
Dillon Adams <dillon-a@hotmail.com>
2018-08-31 23:03:03 -0500

    removing unnecessary assignment

--- Windows/git-tools/gu.bat
+++ Windows/git-tools/gu.bat
@@ -7,8 +7,6 @@ REM
 REM This script is intended to update the current working branch from master (or a specified branch)
 REM
 
-SET TARGET_BRANCH=%1
-
 IF [%1] == [] (
     SET TARGET_BRANCH=master
 ) ELSE (
