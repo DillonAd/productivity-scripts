1493ac2faa027f6b801cbd7988f176abecb84be7
Dillon Adams <dillon-a@hotmail.com>
2018-09-03 14:50:28 -0500

    adding help

--- Windows/add-tool.bat
+++ Windows/add-tool.bat
@@ -4,12 +4,28 @@ SET TOOL=%1
 SET TOOL_DIR=C:\Custom\
 SET PATH_TXT=%PATH%
 
+IF [%1]==[--help] GOTO :help
+IF [%1]==[/?] GOTO :help
+
 IF [%1] == [] (
-    ECHO No tool defined
+    ECHO Error - No tool defined
+    GOTO help
 ) ELSE (
-    IF NOT "x%PATH_TXT%"=="x!PATH_TXT:%TOOL_DIR%=!" ( 
-        ECHO "%PATH%;%TOOL_DIR%"
-        SETX PATH "%PATH%;%TOOL_DIR%" /m
-    )
-    MKLINK %TOOL_DIR%\%TOOL% %CD%\%TOOL% 
+    GOTO main
 )
+
+:help
+
+ECHO Usage: add-tool [tool-name]
+ECHO Options: 
+ECHO   [tool-name]  - Valid executable or batch file
+
+goto end
+
+:main
+IF NOT "x%PATH_TXT%"=="x!PATH_TXT:%TOOL_DIR%=!" ( 
+    SETX PATH "%PATH%;%TOOL_DIR%" /m
+)
+MKLINK %TOOL_DIR%\%TOOL% %CD%\%TOOL% 
+
+:end
