f360e901d8b81a2198eeae344865d233173834b1
Dillon Adams <dillon-a@hotmail.com>
2018-09-02 22:05:05 -0500

    removing debug messages

--- Windows/add-tool.bat
+++ Windows/add-tool.bat
@@ -7,11 +7,9 @@ SET PATH_TXT=%PATH%
 IF [%1] == [] (
     ECHO No tool defined
 ) ELSE (
-    ECHO START
     IF NOT "x%PATH_TXT%"=="x!PATH_TXT:%TOOL_DIR%=!" ( 
         ECHO "%PATH%;%TOOL_DIR%"
         SETX PATH "%PATH%;%TOOL_DIR%" /m
     )
-    ECHO START2
     ROBOCOPY ./ %TOOL_DIR% %TOOL%
 )
