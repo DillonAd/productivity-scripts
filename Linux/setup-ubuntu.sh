8b4f844a72ddadf21d0c1815a513b3a4ecc581cf
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-20 19:53:07 -0500

    Adding Git to setup

--- Linux/setup-ubuntu.sh
+++ Linux/setup-ubuntu.sh
@@ -4,6 +4,11 @@
 sudo apt update
 sudo apt upgrade
 
+# Git
+sudo apt install git
+git config --global user.email "28228453+DillonAd@users.noreply.github.com"
+git config --global user.name "Dillon Adams"
+
 # Install Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
