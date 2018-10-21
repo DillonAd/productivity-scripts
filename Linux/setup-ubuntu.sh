44ba36139e76ae3c648b72802f81172b936c93bc
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-20 20:48:26 -0500

    Adding ZSH/Oh-My-ZSH

--- Linux/setup-ubuntu.sh
+++ Linux/setup-ubuntu.sh
@@ -4,8 +4,10 @@
 sudo apt update
 sudo apt upgrade
 
+sudo apt install curl wget
+
 # Git
-sudo apt install git
+sudo apt install -y git
 git config --global user.email "28228453+DillonAd@users.noreply.github.com"
 git config --global user.name "Dillon Adams"
 
@@ -13,7 +15,7 @@ git config --global user.name "Dillon Adams"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install sublime-text
+sudo apt install -y sublime-text
 
 # Install VS Code
 sudo apt install libcurl4 curl apt-transport-https
@@ -21,19 +23,20 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
-sudo apt install code
+sudo apt install -y code
 
 # Install DotNet Core
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys EB3E94ADBE1229CF
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt update
-sudo apt install dotnet-sdk-2.1.105 
+sudo apt install -y dotnet-sdk-2.1.105
+rm microsoft.gpg
 
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y nodejs
-sudo apt install npm
+sudo apt install -y npm
 
 # Install Docker CE
 sudo apt-get install ca-certificates software-properties-common
@@ -41,14 +44,33 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 
 # Install Slack
  sudo snap install slack --classic
 
  # Install Mono
-
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt install mono-devel
+sudo apt install -y mono-devel
+
+
+# ZSH / Bash Completion
+sudo apt install -y zsh bash-completion
+
+# Git Completion
+GIT_VERSION=`git --version | awk '{print $3}'`
+URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
+curl "$URL" --silent --output "$HOME/.git-completion.bash"
+
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/theme_chooser.sh | sh -s gnzh
+
+# Oh My ZSH - Plugins
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+chsh -s $(which zsh)
