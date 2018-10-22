4c1c3f9bc2382d0369984392d69d39e42a1823da
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-21 19:18:53 -0500

    Adding docker-compose to Ubuntu setup

--- Linux/setup-ubuntu.sh
+++ Linux/setup-ubuntu.sh
@@ -48,6 +48,10 @@ sudo apt-get install -y docker-ce
 
 sudo usermod -aG docker $USER
 
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Install Slack
  sudo snap install slack --classic
 
