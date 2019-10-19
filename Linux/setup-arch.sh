c9964487eacd5c9e1685438a4dcb44b3775d45eb
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-10-18 21:55:26 -0500

    sticking with the docs

--- Linux/setup-arch.sh
+++ Linux/setup-arch.sh
@@ -5,23 +5,20 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   o # clear the in memory partition table
   n # new partition
   p # primary partition
-  1 # partition number 1
+  2 # partition number 1
     # default - start at beginning of disk 
-  +100M # 100 MB boot parttion
-  n # new partition
-  p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
   +3072M  # 3072 MB swap partition
-  3 # partition number 3
+  1 # partition number 3
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
   p # print the in-memory partition table
   w # write the partition table
   q # and we're done
 EOF
+
+  # a # make a partition bootable
+  # 1 # bootable partition is partition 1 -- /dev/sda1
+
 
 # # Format boot partition
 # mkfs.ext4 /dev/sda1
