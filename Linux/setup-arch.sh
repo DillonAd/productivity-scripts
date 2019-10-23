7f2361a1792c3c7d13ef97de70d03cb26fcbf9d6
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-10-22 22:31:44 -0500

    Adding boot partition

--- Linux/setup-arch.sh
+++ Linux/setup-arch.sh
@@ -5,12 +5,17 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   o # clear the in memory partition table
   n # new partition
   p # primary partition
-  2 # partition number 1
+  3 # partition number 3
     # default - start at beginning of disk 
   +3072M  # 3072 MB swap partition
   n # new partition
   p # primary partition
-  1 # partition number 3
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +512M  # 512 MB boot partition
+  n # new partition
+  p # primary partition
+  2 # partition number 2
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
   p # print the in-memory partition table
@@ -19,22 +24,53 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
 EOF
 
 # Encrypt main partition
-cryptsetup luksFormat /dev/sda1
-cryptsetup open /dev/sda1 cryptroot
+cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 /dev/sda2
+cryptsetup open /dev/sda2 cryptroot
 mkfs.ext4 /dev/mapper/cryptroot
 
-# Format and enable swap partition
-mkswap /dev/sda2
-swapon /dev/sda2
-
-# # Mount the main partition
+# Mount the main partition
 mount /dev/mapper/cryptroot /mnt
 
-# # Bootstrap necessary packages
-# pacstrap /mnt base linux linux-firmware
+# Format and enable swap partition
+mkswap /dev/sda3
+swapon /dev/sda3
 
-# # Propagate partition config to disk
-# genfstab -U /mnt >> /mnt/etc/fstab
+# Format and enable boot partiton
+mkfs.fat -F32 /dev/sda1
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 
-# # CHROOT into the new installation
-# arch-chroot /mnt
+# Bootstrap necessary packages
+pacstrap /mnt base linux linux-firmware iproute2 gnome budgie-desktop
+
+# Propagate partition config to disk
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# CHROOT into the new installation
+arch-chroot /mnt
+
+# Set Timezone
+ln -sf /usr/share/zoneinfo/US/Central /etc/localtime
+hwclock --systohc
+
+# Localization
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# Network Configuration
+echo Enter hostname:
+read HOSTNAME
+echo $HOSTNAME > /etc/hostname
+
+echo "
+127.0.0.1	localhost
+::1		    localhost
+127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME"  >> /ect/hosts
+
+# Initramfs configuration
+mkinitcpio -P
+
+# Set root password
+echo Enter new root password
+read NEW_PASS
+echo 'root:$NEW_PASS' | chpasswd
