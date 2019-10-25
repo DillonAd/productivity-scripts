03e328e1d2467db337916123341160df01d8450e
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-10-24 21:19:28 -0500

    switching to gdisk

--- Linux/arch/install.sh
+++ Linux/arch/install.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+pacman -Sy --noconfirm gdisk
+
 # https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
-  o # clear the in memory partition table
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | gdisk /dev/sda
   n # new partition
-  p # primary partition
   1 # partition number 1
     # default - start at beginning of disk 
   +512M  # 512 MB boot partition
-  a # make parition 1 bootable
+  EF00 # specify EFI System partition
   n # new partition
-  p # primary partition
   3 # partition number 3
     # default - start at beginning of disk 
   +3072M  # 3072 MB swap partition
+    # default Linux filesystem
   n # new partition
-  p # primary partition
   2 # partition number 2
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
+    # default Linux filesystem
   p # print the in-memory partition table
   w # write the partition table
   q # and we're done
 EOF
+
+# Format and enable boot partiton
+mkfs.vfat -F32 /dev/sda1
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
 
 # Encrypt main partition
 cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 /dev/sda2
@@ -35,11 +40,6 @@ mount /dev/mapper/cryptroot /mnt
 # Format and enable swap partition
 mkswap /dev/sda3
 swapon /dev/sda3
-
-# Format and enable boot partiton
-mkfs.fat -F 32 /dev/sda1
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
 
 # Bootstrap necessary packages
 pacstrap /mnt base linux linux-firmware
