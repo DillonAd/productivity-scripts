7a327bb80eed769c5d1e5da07be722ec11fc5f6d
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-10-25 21:33:59 -0500

    switching back to fdisk and specifying boot partition format

--- Linux/arch/install.sh
+++ Linux/arch/install.sh
@@ -3,27 +3,29 @@
 pacman -Sy --noconfirm gdisk
 
 # https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | gdisk /dev/sda
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+  o # clear the in memory partition table
   n # new partition
+  p # primary partition
   1 # partition number 1
     # default - start at beginning of disk 
   +512M  # 512 MB boot partition
-  EF00 # specify EFI System partition
-  c # change partition name
-  boot # allocate name to only partition
+  a # make parition 1 bootable
+  t # change partition type
+  b # WD95 FAT32
   n # new partition
+  p # primary partition
   3 # partition number 3
     # default - start at beginning of disk 
   +3072M  # 3072 MB swap partition
-    # default Linux filesystem
   n # new partition
+  p # primary partition
   2 # partition number 2
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
-    # default Linux filesystem
   p # print the in-memory partition table
   w # write the partition table
-  Y # and we're done
+  q # and we're done
 EOF
 
 # Encrypt main partition
