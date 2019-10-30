e9f25873ce5ed346352f4b4c603cd262fa08ed43
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-10-29 20:47:35 -0500

    Removing debug steps for mkinitcpio.conf changes

--- Linux/arch/setup.sh
+++ Linux/arch/setup.sh
@@ -23,8 +23,6 @@ echo "
 echo '' > /etc/vconsole.conf
 
 # mkinitcpio config
-cat /etc/mkinitcpio.conf
-read -p "press any key to continue"
 sed -i s/'HOOKS=([0-9A-Za-z ].*)'/'HOOKS=(base systemd udev autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck)'/ /etc/mkinitcpio.conf
 
 # Initramfs configuration
