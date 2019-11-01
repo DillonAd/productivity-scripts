c3dad5cba16668532f42330ec91914934159c5c5
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-10-31 21:47:53 -0500

    Updating to use GRUB bootloader and try new partition formats and setups

--- Linux/arch/install.sh
+++ Linux/arch/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Update System Clock
+timedatectl set-ntp true
+
 # https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   o # clear the in memory partition table
@@ -8,12 +11,13 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   1 # partition number 1
     # default - start at beginning of disk 
   +512M  # 512 MB boot partition
-  t # change partition type
-  1 # EFI System
+  a # set as boot partition
   n # new partition
   3 # partition number 3
     # default - start at beginning of disk 
   +3072M  # 3072 MB swap partition
+  t # change partition type
+  82 # Linux swap type
   n # new partition
   2 # partition number 2
     # default, start immediately after preceding partition
@@ -27,21 +31,21 @@ cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 /dev/
 cryptsetup open /dev/sda2 cryptroot
 mkfs.ext4 /dev/mapper/cryptroot
 
-# Mount the main partition
-mount /dev/mapper/cryptroot /mnt
-
 # Format and enable boot partiton
-mkfs.vfat -F32 -n BOOT /dev/sda1
+mkfs.ext4 -n BOOT /dev/sda1
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
 
 # Format and enable swap partition
-mkfs.ext4 -L cryptswap /dev/sda3
+#mkfs.ext4 -L cryptswap /dev/sda3
 mkswap /dev/sda3
 swapon /dev/sda3
 
+# Mount the file system
+mount /dev/mapper/cryptroot /mnt
+mount /dev/sda1 /mnt/boot
+
 # Bootstrap necessary packages
-pacstrap /mnt base base-devel linux linux-firmware iproute2 vim wget networkmanager
+pacstrap /mnt base base-devel linux linux-firmware iproute2 vim wget
 
 # Propagate partition config to disk
 genfstab -U /mnt >> /mnt/etc/fstab
--- Linux/arch/setup.sh
+++ Linux/arch/setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Set Timezone
+ln -sf /usr/share/zoneinfo/US/Central /etc/local
 ln -sf /usr/share/zoneinfo/US/Central /etc/localtime
 hwclock --systohc
 
 # Localization
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+sed -i s/'#en_US.UTF-8 UTF-8'/'en_US.UTF-8 UTF-8'/ /etc/locale.gen
+#echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# VConsole Config
+echo '' > /etc/vconsole.conf
 
 # Network Configuration
 echo Enter hostname:
@@ -15,12 +20,17 @@ read HOSTNAME
 echo $HOSTNAME > /etc/hostname
 
 echo "
-127.0.0.1	localhost
-::1		    localhost
-127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME"  >> /etc/hosts
+127.0.0.1	 localhost
+::1		     localhost
+127.0.0.1    $HOSTNAME.localdomain    $HOSTNAME"  >> /etc/hosts
 
-# VConsole Config
-echo '' > /etc/vconsole.conf
+# Network Manager
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+
+# Encrypted Swap
+echo 'swap LABEL=cryptswap /dev/urandom swap,offset=2048,cipher=aes-xts-plain64,size=512' >> /etc/crypttab
+echo '/dev/mapper/swap none swap defaults 0 0' >> /etc/fdisk
 
 # mkinitcpio config
 sed -i s/'HOOKS=([0-9A-Za-z ].*)'/'HOOKS=(base systemd udev autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck)'/ /etc/mkinitcpio.conf
@@ -33,24 +43,21 @@ echo Enter new root password
 read NEW_PASS
 echo 'root:$NEW_PASS' | chpasswd
 
-# Encrypted Swap
-echo 'swap LABEL=cryptswap /dev/urandom swap,offset=2048,cipher=aes-xts-plain64,size=512' >> /etc/crypttab
-echo '/dev/mapper/swap none swap defaults 0 0' >> /etc/fdisk
-
 # Intel microcode
-pacman -Sy --noconfirm intel-ucode
+#pacman -Sy --noconfirm intel-ucode
 
 # Setup systemd
-bootctl install
+# bootctl install
 
-SDA2_UUID=$(blkid -s UUID -o value /dev/sda2)
+# SDA2_UUID=$(blkid -s UUID -o value /dev/sda2)
 
-echo "title Arch Linux
-linux /vmlinuz-linux
-initrd /intel-ucode.img
-initrd /initramfs-linux.img
-options rd.luks.name=${SDA2_UUID}=cryptroot root=/dev/mapper/cryptroot rw" > /boot/loader/entries/arch.conf
+# echo "title Arch Linux
+# linux /vmlinuz-linux
+# initrd /intel-ucode.img
+# initrd /initramfs-linux.img
+# options rd.luks.name=${SDA2_UUID}=cryptroot root=/dev/mapper/cryptroot rw" > /boot/loader/entries/arch.conf
 
-# Network Manager
-systemctl enable NetworkManager
-systemctl start NetworkManager
+# Install GRUB bootloader
+pacman -S grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
