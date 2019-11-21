7d32cfe56c5d88c5c896cd0926bb26bc15202ce3
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-11-21 07:55:33 -0600

    Adding ArchLinux Installation (#12)
    Adding ArchLinux Installation (#12)

---
+++ Linux/arch/1_install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Update System Clock
+timedatectl set-ntp true
+
+# https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+  g # clear the in memory partition table and replace with a gpt table
+  n # new partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +512M  # 512 MB boot partition
+  t # change partition type
+  1 # EFI System type
+  n # new partition
+  3 # partition number 3
+    # default - start at beginning of disk 
+  +4096M  # 4096 MB swap partition
+  t # change partition type
+  3 # partition 3
+  19 # Linux swap type
+  n # new partition
+  2 # partition number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  t # change partition type
+  2 # partition 2
+  20 # Linux filesystem type
+  p # print the in-memory partition table
+  w # write the partition table
+EOF
+
+# Encrypt main partition
+cryptsetup luksFormat --type luks1 --cipher aes-xts-plain64 --key-size 512 /dev/sda2
+cryptsetup open /dev/sda2 cryptroot
+mkfs.ext4 /dev/mapper/cryptroot
+
+# Format and enable boot partiton
+mkfs.fat -F32 -n BOOT /dev/sda1
+mkdir /mnt/boot/EFI
+
+# Format and enable swap partition
+#mkfs.ext4 -L cryptswap /dev/sda3
+mkswap /dev/sda3
+swapon /dev/sda3
+
+# Mount the file system
+mount /dev/mapper/cryptroot /mnt
+mount /dev/sda1 /mnt/boot
+
+# Bootstrap necessary packages
+pacstrap /mnt base base-devel linux linux-firmware iproute2 vim wget grub efibootmgr dosfstools os-prober mtools
+
+# Propagate partition config to disk
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# CHROOT into the new installation
+arch-chroot /mnt
---
+++ Linux/arch/2_setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Set Timezone
+ln -sf /usr/share/zoneinfo/US/Central /etc/local
+ln -sf /usr/share/zoneinfo/US/Central /etc/localtime
+hwclock --systohc
+
+# Localization
+sed -i s/'#en_US.UTF-8 UTF-8'/'en_US.UTF-8 UTF-8'/ /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# VConsole Config
+echo '' > /etc/vconsole.conf
+
+# Network Configuration
+echo Enter hostname:
+read HOSTNAME
+echo $HOSTNAME > /etc/hostname
+
+echo "
+127.0.0.1	 localhost
+::1		     localhost
+127.0.0.1    $HOSTNAME.localdomain    $HOSTNAME"  >> /etc/hosts
+
+# Network Manager
+pacman -Sy --noconfirm networkmanager
+systemctl enable NetworkManager
+
+# Encrypted Swap
+echo 'swap LABEL=cryptswap /dev/urandom swap,offset=2048,cipher=aes-xts-plain64,size=512' >> /etc/crypttab
+echo '/dev/mapper/swap none swap defaults 0 0' >> /etc/fdisk
+
+# Install GRUB bootloader
+#pacman -Sy --noconfirm grub grub-bios
+
+# GRUB ecrypted drive
+sed -i 's#^\(GRUB_CMDLINE_LINUX="\)#GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda2:cryptroot#' /etc/default/grub
+sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
+
+# mkinitcpio config
+sed -i '/^HOOK/s/filesystems/encrypt filesystems/' /etc/mkinitcpio.conf
+
+# Initramfs configuration
+mkinitcpio -p linux
+
+# Intel microcode
+pacman -Sy --noconfirm intel-ucode
+
+# Mount EFI Boot Parition
+mkdir /boot/EFI
+mount /dev/sda1 /boot/EFI
+
+# Configure GRUB bootloader
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --efi-directory=/boot/EFI --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Set root password
+echo Enter new root password
+read NEW_PASS
+echo "root:$NEW_PASS" | chpasswd
+clear
